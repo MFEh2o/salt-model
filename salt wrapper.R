2309c59d75d31b59fbd892e5c74b40dc6d3181c0
Chris Solomon <ctsolomon@users.noreply.github.com>
2023-01-07 17:16:52 -0500

    Bring in code for spatially explicit predictions
    Brough Hilary's existing code from hydrolakeSalt.r into this script to put everything in one place. Minor changes, like slightly altering bins for plotting salt so that 230 mg/L is a breakpoint. Re-produce the map.

--- salt wrapper.R
+++ salt wrapper.R
@@ -1,12 +1,14 @@
 #Simple salt model results
 #CTS 21 Oct 2022
 
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 library(gridExtra)
 library(deSolve)
 library(tigris)
 library(sf)
+library(ggspatial)
+library(MetBrewer)
+library(stars)
 
 source('dSalt.r')
 source('dSaltForce.r')
@@ -281,10 +283,10 @@ alpha <- c(2.5,5,10,15,20,25)#c(0:12)
 delta <- c(0.0001,0.0003,0.0010,0.0030,0.0100,0.0300)
 
 #Precipitation (net of evapotranspiration), m y-1
-#From LakeATLAS data (see 'hydrolakeSalt.r'), take precipitation minus actual evapotranspiration.
-#Values range from <0.02 m to >2 m, but all the really high ones are in the Pacific Northwest.
-#Uaw 0.02, 0.25, 0.50 m y-1 to approximate typical dry, mesic, wet conditions - roughly
-#Montana,  Michigan, Connecticut.
+#From LakeATLAS data (see Model Application 3, below), take precipitation minus actual
+#evapotranspiration. Values range from <0.02 m to >2 m, but all the really high ones are 
+#in the Pacific Northwest. Use 0.02, 0.25, 0.50 m y-1 to approximate typical dry, mesic,
+#wet conditions - roughly Montana,  Michigan, Connecticut.
 p <- c(0.05,0.25,0.50)
 
 
@@ -339,3 +341,107 @@ grid.arrange(g2,g1,nrow=1,widths=c(3,1))
 
 #Close device
 dev.off()
+
+
+#---- Model Application 3: Spatially-explicit predictions of equilibrium road salt concentration ----
+
+# The following code was used to subset LakeATLAS to CONUS
+# hydrolakes.pt = st_read('~/Downloads/LakeATLAS_Data_v10_shp/LakeATLAS_v10_shp/LakeATLAS_v10_pnt_west.shp')
+# 
+# hydrolakes.us = hydrolakes.pt |>
+#   filter(Country == 'United States of America') %>%
+#   dplyr::mutate(lon = sf::st_coordinates(.)[,1],
+#                 lat = sf::st_coordinates(.)[,2]) |>
+#   filter(lat < 50, lon > -150, lat > 22) |> 
+#   dplyr::select(Hylak_id:Wshd_area,starts_with("rdd"), starts_with("pre_mm_u"), starts_with("aet_mm_u"), 
+#          starts_with("pet_mm_u"), starts_with("urb"))
+# 
+# st_write(hydrolakes.us, 'data/LakeATLAS_CONUS.shp', delete_dsn = TRUE)
+
+# Load geospatial data already filtered to parameters of interest (needed to be small enough for GitHub)
+hydrolakes.us = st_read('data/LakeATLAS_CONUS.shp')
+
+#rdd_mk_uav in meters per km², road density in total watershed upstream of lake pour point
+#Note tat this is meters of road per area but NOT lane-m of road per area.
+#pre_mm_uyr precipitation in total watershed upstream of lake pour point annual average
+#aet_mm_uyr actual evapotranspiration in total watershed upstream of lake pour point annual average
+
+
+# Get USGS road salt data from 2015
+r = read_stars('data/USGSsalt/2015.tif') 
+# Transform points to have compatible projection
+a.project = hydrolakes.us |>
+  st_transform(st_crs(r))
+# Extract raster data
+usgsExtract = st_extract(r, a.project) |> 
+  mutate(salt_kg_m2 = `2015.tif` * 0.453592 / 1e6)
+
+# Convert units and add salt 
+b = hydrolakes.us |>
+  mutate(salt_kg_m2 = usgsExtract$salt_kg_m2) |> 
+  mutate(delta = rdd_mk_uav/1e6) |> # convert to meters per m2
+  mutate(p = (pre_mm_uyr - aet_mm_uyr)/1000) #Precipitation (net of evapotranspiration), m y-1
+
+# Check ranges
+range(b$delta)
+range(b$p)
+
+# Just a helpful stackoverflow function to make case_when into factors 
+# https://stackoverflow.com/questions/49572416/r-convert-to-factor-with-order-of-levels-same-with-case-when
+fct_case_when <- function(...) {
+  args <- as.list(match.call())
+  levels <- sapply(args[-1], function(f) f[[3]])  # extract RHS of formula
+  levels <- levels[!is.na(levels)]
+  factor(dplyr::case_when(...), levels=levels)
+}
+
+# Calculate equilibrium concentration of salt in lake (g m-3 = mg L-1)
+# Group by CL concentration
+b <- b %>%
+  # mutate(CL = 10*delta/p*1000) |> 
+  mutate(CL = salt_kg_m2/p*1000) |> 
+  mutate(CL.group = fct_case_when(CL < 10 ~ '0-10',
+                                  CL < 50 ~ '10-50',
+                                  CL < 100 ~ '50-100',
+                                  CL < 230 ~ '100-230',
+                                  CL < 500 ~ '230-500',
+                                  CL >= 500 ~ '500+')) 
+
+# Plots histogram 
+ggplot(b) +
+  geom_histogram(aes(x = CL), fill = 'red4', color = 'black') +
+  scale_y_log10() +
+  xlab("Equlibrium salt concentration"~(mg~Cl^"-"~L^-1)) +
+  theme_bw(base_size = 9) 
+
+## Esri basemap URLs ####
+world_gray <-  paste0('https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/${z}/${y}/${x}.jpeg')
+
+ggplot() +
+  annotation_map_tile(type = world_gray, zoom = 5) + # Esri Basemap
+  geom_sf(data = b, aes(fill = CL.group), size = 1, stroke = 0.2, shape = 21) +
+  scale_fill_manual(values = rev(met.brewer("Peru1", 6)), name=expression(atop(Road~salt,'('*mg~Cl^'-'~L^-1*')'))) +
+  guides(fill=guide_legend(override.aes=list(size=3))) +
+  # scale_fill_gradientn(colors = rev(met.brewer("Peru1"))) +
+  # scale_color_gradientn(colors = rev(met.brewer("Peru1"))) +
+  theme_bw(base_size = 9)
+
+ggsave('figures/saltLakes.jpg', height = 4, width = 6, dpi = 300)  
+
+
+#Plot salt use, does it makes sense?
+test <- b %>%
+  mutate(salt_kg_m2.group = case_when(salt_kg_m2 < 0.005 ~ '0-0.005',
+                                      salt_kg_m2 < 0.01 ~ '0.005-0.01',
+                                      salt_kg_m2 < 0.02 ~ '0.01-0.02',
+                                      salt_kg_m2 < 0.03 ~ '0.02-0.03',
+                                      salt_kg_m2 < 0.05 ~ '0.03-0.05',
+                                      salt_kg_m2 >= 0.05 ~ '0.05+'))
+
+ggplot() +
+  annotation_map_tile(type = world_gray, zoom = 5) + # Esri Basemap
+  geom_sf(data = test, aes(fill = salt_kg_m2.group), size = 1, stroke = 0.2, shape = 21) +
+  scale_fill_manual(values = rev(met.brewer("Peru1", 6)), name = 'Salt (kg/m2)') +
+  theme_bw(base_size = 9)
+ggsave('figures/USGSsalt2015.png', width = 6, height = 4, dpi = 500)
+
